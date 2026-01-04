7e411faa4eca5ed9a2fa283002751cf26ae042f6
dealerofallthecats <xhackerqwertyx@protonmail.com>
2026-01-05 10:13:17 +1300

    Mon 5 Jan 2026 10:13:17 AM

--- modules/display/kde.nix
+++ modules/display/kde.nix
@@ -2,4 +2,11 @@
 
 {
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    kate
+    ark
+    elisa
+    discover
+  ];
 }
