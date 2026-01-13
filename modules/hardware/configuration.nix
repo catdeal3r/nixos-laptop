dc585631346ebdfcf316037f8f24a2195413c73e
dealerofallthecats <xhackerqwertyx@protonmail.com>
2026-01-14 11:24:34 +1300

    Wed 14 Jan 2026 11:24:34 AM

--- modules/hardware/configuration.nix
+++ modules/hardware/configuration.nix
@@ -90,7 +90,7 @@
   };
 
   # Firewall.
-  networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
