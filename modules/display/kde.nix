cd2f5accfe092889fff2884afa8cb433417a2fc8
dealerofallthecats <xhackerqwertyx@protonmail.com>
2026-02-11 16:54:48 +1300

    Wed 11 Feb 2026 04:54:48 PM

--- modules/display/kde.nix
+++ modules/display/kde.nix
@@ -9,17 +9,4 @@
     elisa
     discover
   ];
-  
-  environment.systemPackages = with pkgs; [
-    libsForQt5.qtmultimedia # Or qt6.qtmultimedia
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-base
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-  ];
-  
-  environment.variables = {
-    QT_MEDIA_BACKEND = "gstreamer";
-  };
 }
